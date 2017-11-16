a4e906514287e21b8e12977a5230f0177a310ba5
Krzych <krzych.pierzchala@gmail.com>
2017-11-16 06:54:00 +0100

    Formatted JSX syntax and formatted html file

--- components/Contact.js
+++ components/Contact.js
@@ -1,15 +1,22 @@
 var Contact = React.createClass({
-	propTypes: {
-		item: React.PropTypes.object.isRequired
-	},
-	render: function() {
-		return (
-			<div className={'contactItem'}>
-				<img className={'contactImage'} src={'http://icons.veryicon.com/png/Application/iOS8%20Cirtangle%20Concept/Contacts.png'}/>
-				<p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
-				<p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
-				<a className={'contactEmail'} href={'mailto: ' + this.props.item.email}>{this.props.item.email}</a>
-			</div>
-		);
-	}
+  propTypes: {
+    item: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return (
+      <div className="contactItem">
+        <img
+          className={"contactImage"}
+          src={
+            "http://icons.veryicon.com/png/Application/iOS8%20Cirtangle%20Concept/Contacts.png"
+          }
+        />
+        <p className={"contactLabel"}>Imię: {this.props.item.firstName}</p>
+        <p className={"contactLabel"}>Nazwisko: {this.props.item.lastName}</p>
+        <a className={"contactEmail"} href={"mailto: " + this.props.item.email}>
+          {this.props.item.email}
+        </a>
+      </div>
+    );
+  }
 });
